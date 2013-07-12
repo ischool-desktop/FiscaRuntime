d947f6128d6708dc768e4764b97dae68d3c22435
Gildarts <yaoming.huang@ischool.com.tw>
2013-07-12 15:11:37 +0800

    新增語系編輯器。

---
+++ projects/LangEditor/MainForm.Designer.cs
@@ -0,0 +1,168 @@
+﻿namespace LangEditor
+{
+    partial class MainForm
+    {
+        /// <summary>
+        /// 設計工具所需的變數。
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// 清除任何使用中的資源。
+        /// </summary>
+        /// <param name="disposing">如果應該處置 Managed 資源則為 true，否則為 false。</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form 設計工具產生的程式碼
+
+        /// <summary>
+        /// 此為設計工具支援所需的方法 - 請勿使用程式碼編輯器
+        /// 修改這個方法的內容。
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(MainForm));
+            this.mainmenu = new System.Windows.Forms.MenuStrip();
+            this.mnuFile = new System.Windows.Forms.ToolStripMenuItem();
+            this.mnuOpen = new System.Windows.Forms.ToolStripMenuItem();
+            this.mnuSaveAs = new System.Windows.Forms.ToolStripMenuItem();
+            this.mnuSave = new System.Windows.Forms.ToolStripMenuItem();
+            this.dgvLang = new System.Windows.Forms.DataGridView();
+            this.chKey = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.chValue = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.mnuClose = new System.Windows.Forms.ToolStripMenuItem();
+            this.toolStripMenuItem1 = new System.Windows.Forms.ToolStripSeparator();
+            this.mainmenu.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.dgvLang)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // mainmenu
+            // 
+            this.mainmenu.Items.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.mnuFile});
+            this.mainmenu.Location = new System.Drawing.Point(0, 0);
+            this.mainmenu.Name = "mainmenu";
+            this.mainmenu.Padding = new System.Windows.Forms.Padding(8, 3, 0, 3);
+            this.mainmenu.Size = new System.Drawing.Size(886, 26);
+            this.mainmenu.TabIndex = 0;
+            this.mainmenu.Text = "menuStrip1";
+            // 
+            // mnuFile
+            // 
+            this.mnuFile.DropDownItems.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.mnuOpen,
+            this.mnuSave,
+            this.mnuSaveAs,
+            this.toolStripMenuItem1,
+            this.mnuClose});
+            this.mnuFile.Name = "mnuFile";
+            this.mnuFile.Size = new System.Drawing.Size(44, 20);
+            this.mnuFile.Text = "檔案";
+            // 
+            // mnuOpen
+            // 
+            this.mnuOpen.Name = "mnuOpen";
+            this.mnuOpen.Size = new System.Drawing.Size(152, 22);
+            this.mnuOpen.Text = "Open...";
+            this.mnuOpen.Click += new System.EventHandler(this.mnuOpen_Click);
+            // 
+            // mnuSaveAs
+            // 
+            this.mnuSaveAs.Name = "mnuSaveAs";
+            this.mnuSaveAs.Size = new System.Drawing.Size(152, 22);
+            this.mnuSaveAs.Text = "Save As...";
+            this.mnuSaveAs.Click += new System.EventHandler(this.mnuSaveAs_Click);
+            // 
+            // mnuSave
+            // 
+            this.mnuSave.Name = "mnuSave";
+            this.mnuSave.Size = new System.Drawing.Size(152, 22);
+            this.mnuSave.Text = "Save";
+            this.mnuSave.Click += new System.EventHandler(this.mnuSave_Click);
+            // 
+            // dgvLang
+            // 
+            this.dgvLang.Anchor = ((System.Windows.Forms.AnchorStyles)((((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
+            | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.dgvLang.BackgroundColor = System.Drawing.Color.White;
+            this.dgvLang.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dgvLang.Columns.AddRange(new System.Windows.Forms.DataGridViewColumn[] {
+            this.chKey,
+            this.chValue});
+            this.dgvLang.Location = new System.Drawing.Point(13, 30);
+            this.dgvLang.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
+            this.dgvLang.Name = "dgvLang";
+            this.dgvLang.RowTemplate.Height = 24;
+            this.dgvLang.SelectionMode = System.Windows.Forms.DataGridViewSelectionMode.CellSelect;
+            this.dgvLang.Size = new System.Drawing.Size(860, 623);
+            this.dgvLang.TabIndex = 1;
+            // 
+            // chKey
+            // 
+            this.chKey.HeaderText = "代碼";
+            this.chKey.Name = "chKey";
+            this.chKey.Width = 200;
+            // 
+            // chValue
+            // 
+            this.chValue.AutoSizeMode = System.Windows.Forms.DataGridViewAutoSizeColumnMode.Fill;
+            this.chValue.HeaderText = "資料值";
+            this.chValue.Name = "chValue";
+            // 
+            // mnuClose
+            // 
+            this.mnuClose.Name = "mnuClose";
+            this.mnuClose.Size = new System.Drawing.Size(152, 22);
+            this.mnuClose.Text = "Close";
+            this.mnuClose.Click += new System.EventHandler(this.mnuClose_Click);
+            // 
+            // toolStripMenuItem1
+            // 
+            this.toolStripMenuItem1.Name = "toolStripMenuItem1";
+            this.toolStripMenuItem1.Size = new System.Drawing.Size(149, 6);
+            // 
+            // MainForm
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 17F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(886, 666);
+            this.Controls.Add(this.dgvLang);
+            this.Controls.Add(this.mainmenu);
+            this.Font = new System.Drawing.Font("微軟正黑體", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(136)));
+            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+            this.MainMenuStrip = this.mainmenu;
+            this.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
+            this.Name = "MainForm";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "FISCA 語系編輯器";
+            this.mainmenu.ResumeLayout(false);
+            this.mainmenu.PerformLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.dgvLang)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.MenuStrip mainmenu;
+        private System.Windows.Forms.ToolStripMenuItem mnuFile;
+        private System.Windows.Forms.ToolStripMenuItem mnuOpen;
+        private System.Windows.Forms.ToolStripMenuItem mnuSave;
+        private System.Windows.Forms.ToolStripMenuItem mnuSaveAs;
+        private System.Windows.Forms.DataGridView dgvLang;
+        private System.Windows.Forms.DataGridViewTextBoxColumn chKey;
+        private System.Windows.Forms.DataGridViewTextBoxColumn chValue;
+        private System.Windows.Forms.ToolStripSeparator toolStripMenuItem1;
+        private System.Windows.Forms.ToolStripMenuItem mnuClose;
+    }
+}
+
---
+++ projects/LangEditor/MainForm.cs
@@ -0,0 +1,127 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Resources;
+using System.Text;
+using System.Windows.Forms;
+
+namespace LangEditor
+{
+    public partial class MainForm : Form
+    {
+
+        private string _opened_file;
+
+        private string OpenedFileName
+        {
+            get { return _opened_file; }
+            set
+            {
+                _opened_file = value;
+                Text = string.Format("FISCA 語系編輯器 {0}", _opened_file);
+            }
+        }
+
+
+        public MainForm()
+        {
+            InitializeComponent();
+            OpenedFileName = string.Empty;
+        }
+
+        private void mnuClose_Click(object sender, EventArgs e)
+        {
+            Close();
+        }
+
+        private void mnuOpen_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                OpenFileDialog dialog = new OpenFileDialog();
+                dialog.Filter = "*.resources|*.resources";
+                dialog.Title = "開啟語言檔";
+
+                dgvLang.Rows.Clear();
+                if (dialog.ShowDialog() == System.Windows.Forms.DialogResult.OK)
+                {
+                    ResourceReader rr = new ResourceReader(dialog.FileName);
+                    IDictionaryEnumerator rcset = rr.GetEnumerator();
+                    while (rcset.MoveNext())
+                    {
+                        DataGridViewRow row = new DataGridViewRow();
+                        row.CreateCells(dgvLang, rcset.Key, rcset.Value);
+                        dgvLang.Rows.Add(row);
+                    }
+                    rr.Close();
+                    OpenedFileName = dialog.FileName;
+                }
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show(ex.Message);
+            }
+        }
+
+        private void mnuSave_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                GenerateResourceFile(OpenedFileName);
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show(ex.Message);
+            }
+        }
+
+        private void GenerateResourceFile(string fileName)
+        {
+            ResourceWriter rw = new ResourceWriter(fileName);
+
+            foreach (DataGridViewRow each in dgvLang.Rows)
+            {
+                if (each.IsNewRow)
+                    continue;
+
+                string key = each.Cells[chKey.Name].Value + "";
+                string value = each.Cells[chValue.Name].Value + "";
+
+                rw.AddResource(key, value);
+            }
+            rw.Generate();
+            rw.Close();
+        }
+
+        private void mnuSaveAs_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                List<string> langs = new List<string>();
+                langs.Add("中性(*.resources)|*.resources");
+                langs.Add("英文(*.en-US.resources)|*.en-US.resources");
+                langs.Add("繁體(*.zh-TW.resources)|*.zh-TW.resources");
+                langs.Add("簡體(*.zh-CN.resources)|*.zh-CN.resources");
+                langs.Add("自定(*.*)|*.*");
+
+                SaveFileDialog dialog = new SaveFileDialog();
+                dialog.Filter = string.Join("|", langs.ToArray());
+                dialog.Title = "另存語言檔";
+
+                if (dialog.ShowDialog() == System.Windows.Forms.DialogResult.OK)
+                {
+                    GenerateResourceFile(dialog.FileName);
+                    OpenedFileName = dialog.FileName;
+                }
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show(ex.Message);
+            }
+        }
+    }
+}
