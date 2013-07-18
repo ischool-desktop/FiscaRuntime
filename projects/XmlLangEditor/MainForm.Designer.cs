078005c3e4a8cc23249c0b180f750ccadb1e935c
Gildarts <yaoming.huang@ischool.com.tw>
2013-07-18 14:52:26 +0800

    新增 Xml Language 機制。

---
+++ projects/XmlLangEditor/MainForm.Designer.cs
@@ -0,0 +1,167 @@
+﻿namespace XmlLangEditor
+{
+    partial class MainForm
+    {
+        /// <summary>
+        /// 設計工具所需的變數。
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// 清除任何使用中的資源。
+        /// </summary>
+        /// <param name="disposing">如果應該處置 Managed 資源則為 true，否則為 false。</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form 設計工具產生的程式碼
+
+        /// <summary>
+        /// 此為設計工具支援所需的方法 - 請勿使用程式碼編輯器
+        /// 修改這個方法的內容。
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(MainForm));
+            this.dgv = new System.Windows.Forms.DataGridView();
+            this.menuStrip1 = new System.Windows.Forms.MenuStrip();
+            this.mnuSave = new System.Windows.Forms.ToolStripMenuItem();
+            this.mnuImport = new System.Windows.Forms.ToolStripMenuItem();
+            this.chAssembly = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.chKey = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.chDefault = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.chzhCN = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.chenUS = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.mnuPaste = new System.Windows.Forms.ToolStripMenuItem();
+            ((System.ComponentModel.ISupportInitialize)(this.dgv)).BeginInit();
+            this.menuStrip1.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // dgv
+            // 
+            this.dgv.AllowUserToAddRows = false;
+            this.dgv.AllowUserToDeleteRows = false;
+            this.dgv.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dgv.Columns.AddRange(new System.Windows.Forms.DataGridViewColumn[] {
+            this.chAssembly,
+            this.chKey,
+            this.chDefault,
+            this.chzhCN,
+            this.chenUS});
+            this.dgv.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.dgv.Location = new System.Drawing.Point(0, 24);
+            this.dgv.Margin = new System.Windows.Forms.Padding(4);
+            this.dgv.Name = "dgv";
+            this.dgv.RowTemplate.Height = 24;
+            this.dgv.Size = new System.Drawing.Size(1070, 642);
+            this.dgv.TabIndex = 0;
+            this.dgv.KeyUp += new System.Windows.Forms.KeyEventHandler(this.dgv_KeyUp);
+            // 
+            // menuStrip1
+            // 
+            this.menuStrip1.Items.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.mnuSave,
+            this.mnuImport,
+            this.mnuPaste});
+            this.menuStrip1.Location = new System.Drawing.Point(0, 0);
+            this.menuStrip1.Name = "menuStrip1";
+            this.menuStrip1.Size = new System.Drawing.Size(1070, 24);
+            this.menuStrip1.TabIndex = 1;
+            this.menuStrip1.Text = "menuStrip1";
+            // 
+            // mnuSave
+            // 
+            this.mnuSave.Name = "mnuSave";
+            this.mnuSave.Size = new System.Drawing.Size(44, 20);
+            this.mnuSave.Text = "儲存";
+            this.mnuSave.Click += new System.EventHandler(this.mnuSave_Click);
+            // 
+            // mnuImport
+            // 
+            this.mnuImport.Name = "mnuImport";
+            this.mnuImport.Size = new System.Drawing.Size(44, 20);
+            this.mnuImport.Text = "匯入";
+            this.mnuImport.Visible = false;
+            this.mnuImport.Click += new System.EventHandler(this.mnuImport_Click);
+            // 
+            // chAssembly
+            // 
+            this.chAssembly.HeaderText = "組件";
+            this.chAssembly.Name = "chAssembly";
+            this.chAssembly.ReadOnly = true;
+            // 
+            // chKey
+            // 
+            this.chKey.HeaderText = "代碼";
+            this.chKey.Name = "chKey";
+            this.chKey.ReadOnly = true;
+            this.chKey.Width = 250;
+            // 
+            // chDefault
+            // 
+            this.chDefault.HeaderText = "預設";
+            this.chDefault.Name = "chDefault";
+            this.chDefault.Width = 260;
+            // 
+            // chzhCN
+            // 
+            this.chzhCN.HeaderText = "簡中";
+            this.chzhCN.Name = "chzhCN";
+            this.chzhCN.Width = 260;
+            // 
+            // chenUS
+            // 
+            this.chenUS.HeaderText = "英文";
+            this.chenUS.Name = "chenUS";
+            this.chenUS.Width = 260;
+            // 
+            // mnuPaste
+            // 
+            this.mnuPaste.Name = "mnuPaste";
+            this.mnuPaste.Size = new System.Drawing.Size(44, 20);
+            this.mnuPaste.Text = "貼上";
+            this.mnuPaste.Click += new System.EventHandler(this.mnuPaste_Click);
+            // 
+            // MainForm
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 17F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(1070, 666);
+            this.Controls.Add(this.dgv);
+            this.Controls.Add(this.menuStrip1);
+            this.Font = new System.Drawing.Font("微軟正黑體", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(136)));
+            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+            this.MainMenuStrip = this.menuStrip1;
+            this.Margin = new System.Windows.Forms.Padding(4);
+            this.Name = "MainForm";
+            this.Text = "Xml Language Editor";
+            this.Load += new System.EventHandler(this.MainForm_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.dgv)).EndInit();
+            this.menuStrip1.ResumeLayout(false);
+            this.menuStrip1.PerformLayout();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.DataGridView dgv;
+        private System.Windows.Forms.MenuStrip menuStrip1;
+        private System.Windows.Forms.ToolStripMenuItem mnuSave;
+        private System.Windows.Forms.ToolStripMenuItem mnuImport;
+        private System.Windows.Forms.DataGridViewTextBoxColumn chAssembly;
+        private System.Windows.Forms.DataGridViewTextBoxColumn chKey;
+        private System.Windows.Forms.DataGridViewTextBoxColumn chDefault;
+        private System.Windows.Forms.DataGridViewTextBoxColumn chzhCN;
+        private System.Windows.Forms.DataGridViewTextBoxColumn chenUS;
+        private System.Windows.Forms.ToolStripMenuItem mnuPaste;
+    }
+}
+
---
+++ projects/XmlLangEditor/MainForm.cs
@@ -0,0 +1,175 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Windows.Forms;
+using System.Xml.Linq;
+using System.Xml.XPath;
+
+namespace XmlLangEditor
+{
+    public partial class MainForm : Form
+    {
+        public MainForm()
+        {
+            InitializeComponent();
+        }
+
+        private void MainForm_Load(object sender, EventArgs e)
+        {
+            //string a = Convert.ToBase64String(Encoding.UTF8.GetBytes("1234"));
+
+            try
+            {
+                string fpath = Path.Combine(Application.StartupPath, "Language.xml");
+                XElement xmllang = new XElement("Internationalization");
+                if (File.Exists(fpath))
+                    xmllang = XElement.Load(fpath);
+
+                foreach (XElement asm in xmllang.Elements("Assembly"))
+                {
+                    string asmname = asm.Attribute("Name").Text();
+                    XElement enus = asm.XPathSelectElement("Language[@Code='en-US']");
+                    XElement zhcn = asm.XPathSelectElement("Language[@Code='zh-CN']");
+
+                    foreach (XElement str in asm.XPathSelectElement("Language").Elements("String"))
+                    {
+                        string name = str.Attribute("Name").Text();
+                        string val = str.Value;
+                        string enusVal = enus.XPathSelectElement("String[@Name='" + name + "']").Text();
+                        string zhcnVal = zhcn.XPathSelectElement("String[@Name='" + name + "']").Text();
+
+                        DataGridViewRow row = new DataGridViewRow();
+                        row.CreateCells(dgv, asmname, name, val, zhcnVal, enusVal);
+                        dgv.Rows.Add(row);
+                    }
+                }
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show(ex.Message);
+            }
+        }
+
+        private void mnuPaste_Click(object sender, EventArgs e)
+        {
+            string values = (string)Clipboard.GetData(DataFormats.UnicodeText);
+            DataGridViewCell cell = dgv.CurrentCell;
+
+            if (cell.ColumnIndex <= 1)
+                return;
+
+            dgv.EndEdit();
+
+            StringReader sr = new StringReader(values);
+            int index = cell.RowIndex;
+            while (sr.Peek() >= 0)
+            {
+                dgv.Rows[index].Cells[cell.ColumnIndex].Value = sr.ReadLine();
+                dgv.Rows[index].Cells[cell.ColumnIndex].Selected = true;
+                index++;
+            }
+        }
+
+        private void dgv_KeyUp(object sender, KeyEventArgs e)
+        {
+            if (e.KeyCode == Keys.Delete)
+            {
+                foreach (DataGridViewCell cell in dgv.SelectedCells)
+                {
+                    if (cell.ColumnIndex >= 2)
+                        cell.Value = string.Empty;
+                }
+            }
+        }
+
+        private void mnuSave_Click(object sender, EventArgs e)
+        {
+            XElement xmllang = new XElement("Internationalization");
+
+            foreach (DataGridViewRow row in dgv.Rows)
+            {
+                if (row.IsNewRow) continue;
+
+                string asm = row.Cells[chAssembly.Index].Value + "";
+                string code = row.Cells[chKey.Index].Value + "";
+                string val = row.Cells[chDefault.Index].Value + "";
+                string zhcnV = row.Cells[chzhCN.Index].Value + "";
+                string enusV = row.Cells[chenUS.Index].Value + "";
+
+                XElement asmelm = GetAssemblyElement(xmllang, asm);
+
+                XElement defElm = GetLanguageElement(asmelm, "");
+                XElement zhcnElm = GetLanguageElement(asmelm, "zh-CN");
+                XElement enusElm = GetLanguageElement(asmelm, "en-US");
+
+                defElm.Add(new XElement("String",
+                    new XAttribute("Name", code),
+                    val));
+
+                zhcnElm.Add(new XElement("String",
+                    new XAttribute("Name", code),
+                    zhcnV));
+
+                enusElm.Add(new XElement("String",
+                    new XAttribute("Name", code),
+                    enusV));
+            }
+
+            xmllang.Save(Path.Combine(Application.StartupPath, "Language.xml"));
+        }
+
+        private XElement GetLanguageElement(XElement langs, string code)
+        {
+            XElement lang = langs.XPathSelectElement("Language[@Code='" + code + "']");
+
+            if (lang == null)
+                langs.Add(new XElement("Language", new XAttribute("Code", code)));
+            else
+                return lang;
+
+            return GetLanguageElement(langs, code);
+        }
+
+        private XElement GetAssemblyElement(XElement xmllang, string asmName)
+        {
+            XElement asm = xmllang.XPathSelectElement("Assembly[@Name='" + asmName + "']");
+
+            if (asm == null)
+                xmllang.Add(new XElement("Assembly",
+                    new XAttribute("Name", asmName)));
+            else
+                return asm;
+
+            return GetAssemblyElement(xmllang, asmName);
+        }
+
+        private void mnuImport_Click(object sender, EventArgs e)
+        {
+
+        }
+    }
+
+    internal static class XAttribute_Ext
+    {
+        public static string Text(this XAttribute att)
+        {
+            if (att == null)
+                return string.Empty;
+            else
+                return att.Value;
+        }
+
+        public static string Text(this XElement elm)
+        {
+            if (elm == null)
+                return string.Empty;
+            else
+                return elm.Value;
+        }
+    }
+}
